e2afa08c09be8323deb91941872ce747363844f5
cuuprum <tembagajingga@gmail.com>
2017-05-01 16:07:19 +0700

    Berhasil add button di mainactivithy

--- app/src/main/java/com/dev/cuuprum/gmapsbelajarbareng/MainActivity.java
+++ app/src/main/java/com/dev/cuuprum/gmapsbelajarbareng/MainActivity.java
@@ -1,7 +1,19 @@
 package com.dev.cuuprum.gmapsbelajarbareng;
 
+/*
+* Kelas untuk menu si maps
+ */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
